bb535561d01b3aed11cc284b4eee53af9cc923df
ezzuoya <aaron.zuo@ericsson.com>
2019-09-06 20:42:05 +0800

    modify power modules

--- modules/MM-Modules/MMM-Power/MMM-Power.js
+++ modules/MM-Modules/MMM-Power/MMM-Power.js
@@ -17,10 +17,15 @@ Module.register("MMM-Power", {
 
   getDom: function() {
     var self = this;
+    var monitorOn = function() {
+      window.removeEventListener("dblclick", monitorOn);
+      self.sendNotification("REMOTE_ACTION", { action: "MONITORON" });
+    };
     var wrapper = document.createElement("div");
     wrapper.className = "power_wrapper fa fa-power-off fa-1x";
     wrapper.addEventListener("click", function() {
       self.monitorOff();
+      window.addEventListener("dblclick", monitorOn);
     });
 
     return wrapper;
